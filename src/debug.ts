1cb97b452d867572968d6d059a19f8197894a7e3
memanot <menemty@gmail.com>
2021-09-18 18:34:43 +0300

    debug

---
+++ src/debug.ts
@@ -0,0 +1,13 @@
+import { LowDbBase } from './baseLowDb'
+import { LowDbKv } from './lowdbKv'
+
+const debug = async () => {
+  const db = new LowDbKv({
+    dbName: 'debug'
+  })
+
+  await db.add({ a: new Date() })
+  console.log(await db.get('a'))
+}
+
+debug()
